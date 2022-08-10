a841573980a4d3ab39c519d6726a5dce98afb7d4
BrouteurWasHacked <110950499+BrouteurWasHacked@users.noreply.github.com>
2022-08-10 03:42:20 +0200

    Create brouted.bat

---
+++ brouted.bat
@@ -0,0 +1,11 @@
+@echo off
+color 0a
+mode 1000
+goto start:
+
+:start
+Start
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+PING 1.1.1.1 -n 1 -w 0.3 >NUL
+pause()
+goto start
